5784e0c2a5ef7989daa9fd80596c2ac5cdd92e6a
junghans-kan <jourhans@gmail.com>
2015-05-15 02:02:26 +0900

    for summit

---
+++ html/default.js
@@ -0,0 +1,22 @@
+jQuery(function($){
+	//모바일 GNB 토글 버튼
+	var $gnbBtn = $(".gnb>button");
+	
+	$gnbBtn.on("click", function(){
+		//$(".gnb>ul").slideToggle(200);
+		$(this).next("ul").slideToggle(200);
+
+		if( $gnbBtn.text() ==="메뉴 열기" ){
+			//메뉴 닫기 텍스트
+			$gnbBtn.text("메뉴 닫기");
+		} else {
+			//메뉴 열기 텍스트 표시
+			$gnbBtn.text("메뉴 열기");
+		}
+	});
+
+	// 태블릿 + 데스크톱 GNB 메뉴
+	$(".gnb>ul>li>a").on("mouseover focus", function(){
+		$(this).parent().addClass("active").siblings().removeClass("active");
+	});
+});
